451a39520189bfcc3551ddabf5a4a0713138f945
Davide Tarulli <davidetarulli.dev@gmail.com>
2024-04-16 19:49:23 +0200

    Prova manda e ricevi file con il server

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,8 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, send_file
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
-def hello_world():
-    name = request.args.get("name", "World")
-    return f"Hello, {name}!"
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/generate', methods = ["POST"])
+def generate_pdf():
+    f = request.files["pdftoconvert"]
+    f.save("./temp/pdftoconvert.pdf")
+    return send_file("./temp/pdftoconvert.pdf", as_attachment=True)
