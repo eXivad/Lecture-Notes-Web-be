079dd94d0617dea4fb172f84e87860b1c15217fa
= <=>
2024-04-22 12:07:19 +0200

    Test in one only thread

--- app.py
+++ app.py
@@ -8,20 +8,13 @@ import time
 upload_folder = os.path.join(os.getcwd(), 'temp')
 
 app = Flask(__name__)
-socketio = SocketIO(app)
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-#app.debug = True
+#socketio = SocketIO(app)
+app.debug = True
 
 #Home route
 @app.route('/')
 def home():
     return render_template("index.html")
-
-#API per generare il file pdf
-def generate_and_notify(generator: AnnotatedPDFGenerator):
-    generator.start()
-    generator.join()
-    emit_pdf_ready()
 
 @app.route('/generate', methods = ["POST"])
 def generate_pdf():
@@ -43,8 +36,8 @@ def generate_pdf():
         layout=layout
     )
 
-    thread = threading.Thread(target = generate_and_notify, args=[generator])
-    thread.start()
+    print("Avvio generazione")
+    generator.start()
 
     return redirect(f'/download/page/{file_id}')
 
@@ -52,15 +45,25 @@ def generate_pdf():
 
 @app.route('/download/<file_id>')
 def download(file_id):
-    threading.Thread(target=download_and_delete, args = [file_id]).start()
+    #threading.Thread(target=download_and_delete, args = [file_id]).start()
     return send_file(os.path.join(upload_folder, file_id+'_gen.pdf'), as_attachment=True)
 
 @app.route('/download/page/<file_id>')
 def download_page(file_id):
-    if(os.path.exists(os.path.join(upload_folder, file_id+'_gen.pdf'))):
-        threading.Thread(target=emit_pdf_ready, args=[3]).start()
+    #if(os.path.exists(os.path.join(upload_folder, file_id+'_gen.pdf'))):
+     #   threading.Thread(target=emit_pdf_ready, args=[3]).start()
     return render_template("download.html", id=file_id)
 
+@app.route('/download/check/<file_id>')
+def check_download(file_id):
+    if (os.path.exists(os.path.join(upload_folder, file_id+'_gen.pdf'))):
+        status = 'done'
+    else:
+        status = 'wait'
+
+    return {'status': status}
+
+'''
 def download_and_delete(file_id):
     time.sleep(10)
     os.remove(os.path.join(upload_folder, file_id+'.pdf'))
@@ -69,7 +72,7 @@ def download_and_delete(file_id):
 def emit_pdf_ready(time_sleep=0):
     time.sleep(time_sleep)
     socketio.emit('pdf_ready', {'file': 'done'})
-
+'''
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
 
